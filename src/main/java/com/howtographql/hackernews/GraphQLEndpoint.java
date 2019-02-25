d01f96e6d914570f1bb05307a0fe985c3f5d025a
Ocean Liang <ocean.liang@cargosmart.com>
2019-02-25 16:12:43 +0800

    Initial commit

---
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -0,0 +1,40 @@
+package com.howtographql.hackernews;
+
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import graphql.ExceptionWhileDataFetching;
+import graphql.GraphQLError;
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.GraphQLContext;
+import graphql.servlet.SimpleGraphQLServlet;
+
+/**
+ * The servlet acting as the GraphQL endpoint
+ */
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    private static GraphQLSchema buildSchema() {
+        LinkRepository linkRepository = new LinkRepository();
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(new Query(linkRepository))
+                .build()
+                .makeExecutableSchema();
+    }
+}
---
+++ src/main/java/com/howtographql/hackernews/LinkRepository.java
@@ -0,0 +1,39 @@
+package com.howtographql.hackernews;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.regex;
+
+/**
+ * Manages link persistence
+ */
+public class LinkRepository {
+
+    private final List<Link> links;
+
+    public LinkRepository() {
+        links = new ArrayList<>();
+        //add some links to start off with
+        links.add(new Link("http://howtographql.com", "Your favorite GraphQL page"));
+        links.add(new Link("http://graphql.org/learn/", "The official docks"));
+    }
+
+    public List<Link> getAllLinks() {
+        return links;
+    }
+
+    public void saveLink(Link link) {
+        links.add(link);
+    }
+}
